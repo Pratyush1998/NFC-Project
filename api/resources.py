ac5d8033b28f103bd17050714cdd9f4fb7794e87
Pratyush Bhandari <bhandarp@mcmaster.ca>
2018-07-10 16:01:10 -0400

    first upload

---
+++ api/resources.py
@@ -0,0 +1,16 @@
+from tastypie.resources import ModelResource
+from api.models import Note
+class NoteResource(ModelResource):
+    class Meta:
+        queryset = Note.objects.all()
+        resource_name = 'note'
+
+
+from tastypie.resources import ModelResource
+from api.models import Note
+from tastypie.authorization import Authorization
+class NoteResource(ModelResource):
+    class Meta:
+        queryset = Note.objects.all()
+        resource_name = 'note'
+        authorization = Authorization()
