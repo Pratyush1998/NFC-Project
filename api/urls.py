fa367dde891b77b491448f5ab3c7a71e2a26776a
Pratyush Bhandari <bhandarp@mcmaster.ca>
2019-01-26 23:14:55 -0500

    making changes to url file

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from . import views
 from api.resources import CustomerResource
 from api.resources import ItemResource
@@ -8,6 +8,6 @@ item_resource = ItemResource()
 
 urlpatterns = [
     url(r'^$', views.index, name = 'index'),
-    url(r'^customer/', customer_resource.urls),
-    url('r^item/', item_resource.urls)
+    url(r'^customer/', include(customer_resource.urls)),
+    url('r^item/', include(item_resource.urls))
 ]
