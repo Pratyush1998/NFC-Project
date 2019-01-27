440f2da3f253c6f83c6121ad10f1d9697e5d2bb5
Pratyush Bhandari <bhandarp@mcmaster.ca>
2019-01-26 21:51:19 -0500

    Updating settings.py file

--- notable_django/settings.py
+++ notable_django/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_8!dyknnpnd6p6d6&jl56(^!8xphnxftl8qjl6t^d6hcgi35j&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
