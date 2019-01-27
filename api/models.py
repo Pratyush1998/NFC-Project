e27e5027194214046b3f71f8ddbe1851f6c2654c
Pratyush Bhandari <bhandarp@mcmaster.ca>
2019-01-27 02:17:03 -0500

    fixing urls

--- api/models.py
+++ api/models.py
@@ -2,15 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Customer(models.Model):
-    location = models.CharField(max_length=200)
-
-    def _str_(self):
-        return '%s' % (self.location)
-
-
-class Items(models.Model):
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    #location = models.CharField(max_length=200)
     sku = models.IntegerField()
 
     def _str_(self):
-        return "%s" % (self.sku)
+        return '%s' % (self.sku)
